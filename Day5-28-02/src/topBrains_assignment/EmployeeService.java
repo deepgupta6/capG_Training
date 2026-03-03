eef98b040930d9469c1e7a7d506cbb5562f3df3d
Deep Gupta <deep2004gupta@gmail.com>
2026-03-03 09:45:02 +0530

    added assignments for day6

--- Day5-28-02/src/topBrains_assignment/EmployeeService.java
+++ Day5-28-02/src/topBrains_assignment/EmployeeService.java
@@ -8,8 +8,6 @@ import java.util.stream.Collectors;
 
 public class EmployeeService {
 
-	
-
 	// Ques1
 	public Double avgSalary() {
 
@@ -20,48 +18,35 @@ public class EmployeeService {
 
 	// Ques 2
 	public Map<Department, Employee> maxSalaryPerDepartment() {
-		Map<Department,Employee> m = EmployeeRepository.getEmployees().stream()
-	            .filter(e -> e.getDepartment() != null)
-	            .collect(Collectors.groupingBy(
-	                    Employee::getDepartment,
-	                    Collectors.collectingAndThen(
-	                            Collectors.maxBy(
-	                                    Comparator.comparing(Employee::getSalary)
-	                            ),
-	                            Optional::get
-	                    )
-	            ));
+		Map<Department, Employee> m = EmployeeRepository.getEmployees().stream().filter(e -> e.getDepartment() != null)
+				.collect(Collectors.groupingBy(e->e.getDepartment(), Collectors.collectingAndThen(
+						Collectors.maxBy(Comparator.comparing(Employee::getSalary)), Optional::get)));
 
-	    return m;
-	}
-	
-	//Ques3
-	
-	public Map<Department, Double> totalSalaryPerDepartment() {
-
-		Map<Department,Double> m = EmployeeRepository.getEmployees().stream()
-	            .filter(e -> e.getDepartment() != null)
-	            .collect(Collectors.groupingBy(
-	                    Employee::getDepartment,
-	                    Collectors.summingDouble(Employee::getSalary)
-	            ));
 		return m;
 	}
-	
+
+	// Ques3
+
+	public Map<Department, Double> totalSalaryPerDepartment() {
+
+		Map<Department, Double> m = EmployeeRepository.getEmployees().stream().filter(e -> e.getDepartment() != null)
+				.collect(Collectors.groupingBy(e->e.getDepartment(), Collectors.summingDouble(e->e.getSalary())));
+		return m;
+	}
+
 	// Ques4
 	public Map<String, Double> display() {
-	    Map<String, Double> m = EmployeeRepository.getEmployees().stream()
-	            .collect(Collectors.toMap(
-	                    Employee::getFirstName,
-	                    Employee::getSalary
-	            )); 
-	    return m;
+		Map<String, Double> m = EmployeeRepository.getEmployees().stream()
+				.collect(Collectors.toMap(e->e.getFirstName(), e->e.getSalary()));
+		return m;
 	}
-	
-	//Ques5
-	
-	public List<Double> sortedSal(){
-		List<Double> li = EmployeeRepository.getEmployees().stream().sorted((e1,e2)->Double.compare(e1.getSalary(), e2.getSalary())).map(e->e.getSalary()).collect(Collectors.toList());
+
+	// Ques5
+
+	public List<Double> sortedSal() {
+		List<Double> li = EmployeeRepository.getEmployees().stream()
+				.sorted((e1, e2) -> Double.compare(e2.getSalary(),e1.getSalary())).map(e -> e.getSalary())
+				.toList();
 		return li;
 	}
 
